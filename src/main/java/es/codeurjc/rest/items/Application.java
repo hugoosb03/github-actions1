72a82942edc28cd361508113b1e4f3eacad82683
Hugo <hugoosb03@gmail.com>
2022-05-04 15:22:19 +0200

    Add a comment on app

--- src/main/java/es/codeurjc/rest/items/Application.java
+++ src/main/java/es/codeurjc/rest/items/Application.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
 
+	
+	//Un comentario en la aplicacion
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
